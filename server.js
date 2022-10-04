2bab5fe310b94926e4f7aa5970b3bc212d7dee35
myacademic17 <113384210+myacademic17@users.noreply.github.com>
2022-10-04 16:13:32 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -9,12 +9,17 @@ function onHttpStart(){
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "/pages/home.html"));
-  });
+app.get("/", function(req, res){
+    let resText = "<h2>Declaration</h2>";
+    resText += "I declare that this test is in accordance with Seneca Academic Policy. No part of this test has been copied manually or electronically from any other source.";
+    resText += "Name: <mark>Humaira Shaikh</mark>";
+    resText += "Student Number: <mark>139877203</mark>";
+    resText +=  "<a href = './test1.html'> <u>Go to Test1</u></a> "; 
+    res.send(resText);
+});
 
-  app.get('/test1', function (req, res) {
+app.get("/test1", function(req, res){
     res.sendFile(path.join(__dirname, "/pages/test1.html"));
-})
+});
 
 app.listen(HTTP_PORT, onHttpStart);
